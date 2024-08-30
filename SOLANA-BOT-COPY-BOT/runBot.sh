df5909392c3cc95cc105719d37b6277a2417fbb6
Neo <tyler.durette@gmail.com>
2024-08-30 12:43:11 -0400

    working cron run sh

--- SOLANA-BOT-COPY-BOT/runBot.sh
+++ SOLANA-BOT-COPY-BOT/runBot.sh
@@ -7,14 +7,16 @@
 # USE .SH AS CRON JOB FILE
 # */30 * * * * /path/to/node /path/to/your/script.js <walletAddress>
 
-#ts-node check_wallet.ts 6ZDdVLFc2CRaPUwUGnfUsmXr32EBWoNaM9Axf5LWUjc6
-npx ts-node monitor_wallet.ts 6ZDdVLFc2CRaPUwUGnfUsmXr32EBWoNaM9Axf5LWUjc6
+source /Users/neo/.zshrc
+
+npx ts-node $HOME/SOLANA-BOT-COPY-BOT/monitor_wallet.ts 6ZDdVLFc2CRaPUwUGnfUsmXr32EBWoNaM9Axf5LWUjc6
+#/Users/neo/.nvm/versions/node/v20.17.0/bin/npx ts-node monitor_wallet.ts 6ZDdVLFc2CRaPUwUGnfUsmXr32EBWoNaM9Axf5LWUjc6
 
 # Check if updated_tokens.json exists in the data directory
 # Add logic for buying or selling
-if [ -f "./data/updated_tokens.json" ]; then
+if [ -f "$HOME/SOLANA-BOT-COPY-BOT/data/updated_tokens.json" ]; then
     echo "updated_tokens.json exists, running follow-up script..."
-    python swap.py
+    python $HOME/SOLANA-BOT-COPY-BOT/swap.py
     # rm data/updated_tokens.json
 else
     echo "No updates found, follow-up script will not run."
