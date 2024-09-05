09602e90d859d86bd5d7b8c7376756ead389cd54
Tyler Durette <tyler.durette@gmail.com>
2024-09-05 17:52:03 -0400

    im dumb

--- SOLANA-BOT-SNIPER-BOT-FINAL/sell_token_500.py
+++ SOLANA-BOT-SNIPER-BOT-FINAL/sell_token_500.py
@@ -80,7 +80,7 @@ params = {
     'inputMint': mint_address,
     'outputMint': 'So11111111111111111111111111111111111111112',  # Converting to SOL
     'amount': sell_amount_raw,  # Sell 50% of tokens
-    'slippageBps': '350'  # 1% slippage
+    'slippageBps': '999'  # 1% slippage
 }
 
 try:
