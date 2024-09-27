db62af9675feb1672c3bb0f0830c9d778c595256
bestisblessed <tyler.durette@gmail.com>
2024-09-26 23:27:11 -0400

    im dumb

--- SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/sell_token_80.py
+++ SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/sell_token_80.py
@@ -206,7 +206,7 @@ params = {
     'inputMint': mint_address,
     'outputMint': 'So11111111111111111111111111111111111111112',  # Converting to SOL
     'amount': sell_amount_raw,  # Sell 80% of tokens
-    'slippageBps': '700'  # 1% slippage
+    'slippageBps': '500'  # 1% slippage
 }
 
 try:
