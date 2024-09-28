08b97bf909fa597235a27a444e07278ab248f851
Neo <tyler.durette@gmail.com>
2024-09-28 17:10:43 -0400

    sell 500

--- SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/sell_token_500.py
+++ SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/sell_token_500.py
@@ -207,7 +207,7 @@ params = {
     'inputMint': mint_address,
     'outputMint': 'So11111111111111111111111111111111111111112',  # Converting to SOL
     'amount': sell_amount_raw,  # Sell 80% of tokens
-    'slippageBps': '1000'  # 1% slippage
+    'slippageBps': '500'  # 1% slippage
 }
 
 time.sleep(2)
