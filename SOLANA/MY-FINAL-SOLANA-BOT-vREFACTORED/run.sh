20808a7e0d277270e3caa546913c11f97ce672a0
Neo <tyler.durette@gmail.com>
2024-09-28 14:06:12 -0400

    run.sh

--- SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/run.sh
+++ SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/run.sh
@@ -6,3 +6,8 @@ rm new_tokens/*
 rm wallets/*
 
 python monitor_wallet.py
+
+# Start a new screen session called 'bot' and run node SCANNER.js
+screen -S bot -dm bash -c 'node SCANNER.js | tee -a scanner.log'
+
+echo "Started 'bot' screen session. You can view it by running: screen -r bot"
