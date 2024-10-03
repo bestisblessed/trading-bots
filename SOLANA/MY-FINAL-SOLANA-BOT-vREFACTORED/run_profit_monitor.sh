41086d619f9c2273e635267a1982744f9a0f6db5
Neo <tyler.durette@gmail.com>
2024-10-02 23:05:02 -0400

    im dumb

--- SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/run_profit_monitor.sh
+++ SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/run_profit_monitor.sh
@@ -4,7 +4,7 @@
 /Users/neo/.pyenv/shims/python /Users/neo/trading-bots/SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/profit_monitor.py >> /Users/neo/trading-bots/SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/profit_monitor.log 2>&1
 
 # Wait for 30 seconds
-sleep 30
+sleep 40
 
 # Run the script again
 /Users/neo/.pyenv/shims/python /Users/neo/trading-bots/SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/profit_monitor.py >> /Users/neo/trading-bots/SOLANA/MY-FINAL-SOLANA-BOT-vREFACTORED/profit_monitor.log 2>&1
